a98ead11890b21bfd67982cbb7808bf42b52834d
Gabriel M. Wolf <92558787+gwolf97@users.noreply.github.com>
2022-01-05 15:53:09 -0500

    Simple features for a memory card game

---
+++ js/main.js
@@ -0,0 +1,106 @@
+const boardGame = document.querySelector(".board")
+const board = [
+    {
+        name: 'bulbasaur',
+        img: 'img/bulbasaur.png'
+    },
+    {
+        name: 'bulbasaur',
+        img: 'img/bulbasaur.png'
+    },
+    {
+        name: 'charmander',
+        img: 'img/charmander.png'
+    },
+    {
+        name: 'charmander',
+        img: 'img/charmander.png'
+    },
+    {
+        name: 'dialga',
+        img: 'img/dialga.png'
+    },
+    {
+        name: 'dialga',
+        img: 'img/dialga.png'
+    },
+    {
+        name: 'mew',
+        img: 'img/mew.png'
+    },
+    {
+        name: 'mew',
+        img: 'img/mew.png'
+    },
+    {
+        name: 'pikachu',
+        img: 'img/pikachu.png'
+    },
+    {
+        name: 'pikachu',
+        img: 'img/pikachu.png'
+    },
+    {
+        name: 'rayquaza',
+        img: 'img/rayquaza.png'
+    },
+    {
+        name: 'rayquaza',
+        img: 'img/rayquaza.png'
+    },
+]
+
+board.sort(() => 0.5 - Math.random())
+
+
+let cardsChosen = []
+let cardsChosenId = []
+let scoreKeep = []
+
+
+const createBoard = function(){
+    for(let object of board){
+        const card = document.createElement("img")
+        card.setAttribute('src', 'img/pokeball.png')
+        card.setAttribute('data-id', board.indexOf(object))
+        card.addEventListener("click", flipCard)
+        boardGame.append(card)
+        
+    }
+}
+
+createBoard()
+
+function flipCard(){
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(board[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', board[cardId].img)
+    if (cardsChosen.length === 2){
+       setTimeout(checkMatch, 500)
+    }
+
+}
+
+function checkMatch(){
+    let cards = document.querySelectorAll('img')
+    const card1 = cardsChosenId[0]
+    const card2 = cardsChosenId[1]
+    if (cardsChosen[0] === cardsChosen[1]){
+        cards[card1].setAttribute('src', 'img/black.jpeg')
+        cards[card2].setAttribute('src', 'img/black.jpeg')
+        const score = document.querySelector(".score")
+        scoreKeep.push(cardsChosen)
+        score.innerHTML = scoreKeep.length
+        
+    }else{
+        cards[card1].setAttribute('src', 'img/pokeball.png')
+        cards[card2].setAttribute('src', 'img/pokeball.png')
+    }
+    cardsChosen = []
+    cardsChosenId = [] 
+    
+    if (scoreKeep.length === board.length/2){
+        alert('Congatulations! You found them all!')
+    }
+}
